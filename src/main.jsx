7ead8ac5460dd40eff5f2a0484c9be155c44a8dd
Exteban08 <exteban16@gmail.com>
2023-02-02 23:34:23 -0600

    React-Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
